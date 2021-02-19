6809c5e7919c13b5633bb6358576c3c942e64e53
Lelberto <jsurieux@gmail.com>
2021-02-19 19:24:48 +0100

    Add canvas The canvas is used to render an army

---
+++ src/components/canvas.tsx
@@ -0,0 +1,45 @@
+import { FC, HTMLProps, useEffect, useRef } from 'react';
+import { ArmyData } from '../types/data-types';
+
+/**
+ * Canvas props.
+ */
+export interface CanvasProps extends HTMLProps<HTMLCanvasElement> {
+  army: ArmyData;
+}
+
+/**
+ * Canvas component.
+ * 
+ * The canvas can draw an army.
+ * 
+ * @param army Army to draw 
+ */
+export const Canvas: FC<CanvasProps> = ({ army, ...rest }) => {
+  const canvasRef = useRef<HTMLCanvasElement>();
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    let animationFrameId: number;
+
+    const render = () => {
+      context.fillStyle = '#000';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      for (const entity of army.entities) {
+        context.fillStyle = entity.color;
+        context.fillRect(entity.position.x, entity.position.y, entity.size.width, entity.size.height);
+      }
+      animationFrameId = window.requestAnimationFrame(render);
+    }
+    render();
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    }
+  }, []);
+
+  return (
+    <canvas ref={canvasRef} {...rest} />
+  );
+}
